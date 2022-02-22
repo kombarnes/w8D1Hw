549e0be81afece054f9a47d9b0c95dbd25b44b03
Kourtney <barnes.kourt@gmail.com>
2022-02-21 20:20:00 -0500

    hw

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['Kourtney', 'Dahmere', 'Sua', 'Darsh'];
+const ages = [23, 25, 22, 24];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+// const name = 'Kourtney';
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('Kourtney');
+greet('Dahmere');
